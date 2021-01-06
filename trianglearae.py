d8b8487b6fd34593323a4ac34c9fe6de78b64f18
GouriManoj <70144037+GouriManoj@users.noreply.github.com>
2021-01-06 12:13:14 +0530

    Add files via upload

---
+++ trianglearae.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 12:09:45 2021
+
+@author: Gouri Manoj
+"""
+
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
+
+area = b*h/2
+
+print("area = ", area)
