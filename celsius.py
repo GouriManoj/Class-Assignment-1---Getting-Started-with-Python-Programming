07ab6a3f0751c2f4fadd99c5bef44e9690c50686
GouriManoj <70144037+GouriManoj@users.noreply.github.com>
2021-01-06 12:05:32 +0530

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 12:04:25 2021
+
+@author: Gouri Manoj
+"""
+
+celsius=int(input("Enter the temperature in celcius:"))
+f=(celsius*1.8)+32
+print("Temperature in farenheit is:",f)
