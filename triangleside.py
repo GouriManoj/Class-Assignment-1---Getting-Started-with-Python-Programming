96c92883ba6d7a085fb8bac3b63e6c381aff60ef
GouriManoj <70144037+GouriManoj@users.noreply.github.com>
2021-01-06 11:56:19 +0530

    Add files via upload

---
+++ triangleside.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 11:51:54 2021
+
+@author: Gouri Manoj
+"""
+
+x=int(input("Enter first side "))
+y=int(input("Enter second side "))
+z=int(input("Enter third side "))
+if x**2+y**2==z**2:
+    print('Yes')
+else:
+    print('No')
